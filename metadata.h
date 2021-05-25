7e0d0d379fa119624cf8683134c76ebb6c3a30fa
Vitalii Aleksashin <zenix@cn.ru>
2021-05-25 14:37:07 +0700

    fix program name

--- metadata.h
+++ metadata.h
@@ -1,7 +1,7 @@
 #ifndef METADATA_H
 #define METADATA_H
 
-#define APP_NAME                "cchbot"
+#define APP_NAME                "ccshbot"
 #define ORGANIZATION_STRING     "valeksan-soft"
 #define DOMAIN_STRING           "ru.valeksansoft"
 
